f5d080297d577fe4390d102ac305ca35de1e3b76
katescha8 <kate.schapo.5@gmail.com>
2023-03-12 08:30:59 +0300

    Добавьте файлы проекта.

---
+++ FontSetting.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Notepad1
+{
+    public partial class FontSettings : Form
+        {
+            public int FontSize = 0;
+            public System.Drawing.FontStyle fs = FontStyle.Regular;
+
+            public FontSettings()
+            {
+                InitializeComponent();
+                FontBox.SelectedItem = FontBox.Items[0];
+                StyleBox.SelectedItem = StyleBox.Items[0];
+            }
+
+            private void OnFontChanged(object sender, EventArgs e)
+            {
+                ExampleText.Font = new Font(ExampleText.Font.FontFamily, int.Parse(FontBox.SelectedItem.ToString()), ExampleText.Font.Style);
+                FontSize = int.Parse(FontBox.SelectedItem.ToString());
+            }
+
+            private void OnStyleChanged(object sender, EventArgs e)
+            {
+                switch (StyleBox.SelectedItem.ToString())
+                {
+                    case "обычный":
+                        ExampleText.Font = new Font(ExampleText.Font.FontFamily, int.Parse(FontBox.SelectedItem.ToString()), FontStyle.Regular);
+                        break;
+                    case "курсив":
+                        ExampleText.Font = new Font(ExampleText.Font.FontFamily, int.Parse(FontBox.SelectedItem.ToString()), FontStyle.Italic);
+                        break;
+                    case "полужирный":
+                        ExampleText.Font = new Font(ExampleText.Font.FontFamily, int.Parse(FontBox.SelectedItem.ToString()), FontStyle.Bold);
+                        break;
+                    case "линия по середине":
+                        ExampleText.Font = new Font(ExampleText.Font.FontFamily, int.Parse(FontBox.SelectedItem.ToString()), FontStyle.Strikeout);
+                        break;
+                    case "подчеркивание":
+                        ExampleText.Font = new Font(ExampleText.Font.FontFamily, int.Parse(FontBox.SelectedItem.ToString()), FontStyle.Underline);
+                        break;
+
+                }
+                fs = ExampleText.Font.Style;
+            }
+
+            private void button1_Click(object sender, EventArgs e)
+            {
+                this.Hide();
+            }
+        }
+    }
---
+++ Notes.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Notepad1
+{
+    public partial class Notes : Form
+        {
+            public int FontSize = 0;
+            public System.Drawing.FontStyle fs = FontStyle.Regular;
+
+            public string filename;
+            public bool isFileChanged;
+
+            public FontSettings fontsetts;
+            public Notes()
+            {
+                InitializeComponent();
+
+                Init();
+            }
+            public void Init()
+            {
+                filename = "";
+                isFileChanged = false;
+                UpdateTextWithTitle();
+            }
+            public void CreateNewDocument(object sender, EventArgs e)
+            {
+                SaveUnsavedFile();
+                textBox1.Text = "";
+                filename = "";
+                isFileChanged = false;
+                UpdateTextWithTitle();
+            }
+            public void OpenFile(object sender, EventArgs e)
+            {
+                SaveUnsavedFile();
+                openFileDialog1.FileName = "";
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        StreamReader streamr = new StreamReader(openFileDialog1.FileName);
+                        textBox1.Text = streamr.ReadToEnd();
+                        streamr.Close();
+                        filename = openFileDialog1.FileName;
+                        isFileChanged = false;
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Невозможно открыть файл!");
+                    }
+                }
+                UpdateTextWithTitle();
+            }
+            public void SaveFile(string _filename)
+            {
+                if (_filename == "")
+                {
+                    if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                    {
+                        _filename = saveFileDialog1.FileName;
+                    }
+                }
+                try
+                {
+                    StreamWriter streamw = new StreamWriter(_filename + ".txt");
+                    streamw.Write(textBox1.Text);
+                    streamw.Close();
+                    filename = _filename;
+                    isFileChanged = false;
+                }
+                catch
+                {
+                    MessageBox.Show("Невозможно сохранить файл!");
+                }
+            }
+            public void Save(object sender, EventArgs e)
+            {
+                SaveFile(filename);
+            }
+            public void SaveAs(object sender, EventArgs e)
+            {
+                SaveFile("");
+            }
+
+            private void OnTextChanged(object sender, EventArgs e)
+            {
+                if (!isFileChanged)
+                {
+                    this.Text = this.Text.Replace('*', ' ');
+                    isFileChanged = true;
+                    this.Text = "*" + this.Text;
+                }
+            }
+            public void UpdateTextWithTitle()
+            {
+                if (filename != "")
+                    this.Text = filename + " - Блокнот";
+                else this.Text = filename + "Безымянный - Блокнот";
+            }
+            public void SaveUnsavedFile()
+            {
+                if (isFileChanged)
+                {
+                    DialogResult result = MessageBox.Show("Сохранить изменения в файле?", "Сохранение файла", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1);
+                    if (result == DialogResult.Yes)
+                    {
+                        SaveFile(filename);
+                    }
+                }
+            }
+            public void CopyText()
+            {
+                Clipboard.SetText(textBox1.SelectedText);
+            }
+            public void CutText()
+            {
+                Clipboard.SetText(textBox1.SelectedText);
+                textBox1.Text = textBox1.Text.Remove(textBox1.SelectionStart, textBox1.SelectionLength);
+            }
+            public void PasteText()
+            {
+                textBox1.Text = textBox1.Text.Substring(0, textBox1.SelectionStart) + Clipboard.GetText() + textBox1.Text.Substring(textBox1.SelectionStart, textBox1.Text.Length - textBox1.SelectionStart);
+            }
+            private void правкаToolStripMenuItem_Click(object sender, EventArgs e)
+            {
+            }
+            private void файлToolStripMenuItem_Click(object sender, EventArgs e)
+            {
+            }
+            private void OnCopyClick(object sender, EventArgs e)
+            {
+                CopyText();
+            }
+            private void OnCutClick(object sender, EventArgs e)
+            {
+                CutText();
+            }
+            private void OnPasteClick(object sender, EventArgs e)
+            {
+                PasteText();
+            }
+            private void OnFormClosing(object sender, FormClosingEventArgs e)
+            {
+                SaveUnsavedFile();
+            }
+            private void OnFontClick(object sender, EventArgs e)
+            {
+                fontsetts = new FontSettings();
+                fontsetts.Show();
+            }
+
+            private void OnFocus(object sender, EventArgs e)
+            {
+                if (fontsetts != null)
+                {
+                    FontSize = fontsetts.FontSize;
+                    fs = fontsetts.fs;
+                    textBox1.Font = new Font(textBox1.Font.FontFamily, FontSize, fs);
+                    fontsetts.Close();
+                }
+            }
+        }
+    }
